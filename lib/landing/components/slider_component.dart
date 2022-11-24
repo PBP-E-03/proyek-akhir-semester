05132dbdd683ab13c5ba64498037d361a423293c
DJaegerScript <adjiedjaka0902@gmail.com>
2022-11-24 10:55:48 +0700

    created landing page

---
+++ lib/landing/components/slider_component.dart
@@ -0,0 +1,58 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class SliderComponent extends StatefulWidget {
+  const SliderComponent({super.key, required this.items});
+
+  final List<Widget> items;
+
+  @override
+  State<SliderComponent> createState() => _SliderComponentState();
+}
+
+class _SliderComponentState extends State<SliderComponent> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider(
+          items: widget.items,
+          carouselController: _controller,
+          options: CarouselOptions(
+              height: MediaQuery.of(context).size.height * 0.725,
+              viewportFraction: 1.0,
+              enlargeCenterPage: false,
+              autoPlay: true,
+              enableInfiniteScroll: true,
+              autoPlayInterval: const Duration(seconds: 8),
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _current = index;
+                });
+              }),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: widget.items.asMap().entries.map((entry) {
+            return GestureDetector(
+              onTap: () => _controller.animateToPage(entry.key),
+              child: Container(
+                width: 12.0,
+                height: 12.0,
+                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: (Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.black)
+                        .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+              ),
+            );
+          }).toList(),
+        ),
+      ],
+    );
+  }
+}
